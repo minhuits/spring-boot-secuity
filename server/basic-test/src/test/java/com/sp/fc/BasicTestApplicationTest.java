0ba0833d8c4c2d82a048d569f73f4781cf7f7c44
ksksks <a41417144@gmail.com>
2022-09-19 23:27:11 +0900

    chapter01. 강의소개
    - gradle 프로젝트 설정

---
+++ server/basic-test/src/test/java/com/sp/fc/BasicTestApplicationTest.java
@@ -0,0 +1,14 @@
+package com.sp.fc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ApplicationTest {
+    @DisplayName("1. Test")
+    @Test
+    void Test() {
+        assertEquals("test", "test");
+    }
+}
