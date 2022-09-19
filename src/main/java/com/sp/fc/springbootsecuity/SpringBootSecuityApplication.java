c411cc4eff7fd0545dd3dd14e21a5a4b61ce4d00
ksksks <a41417144@gmail.com>
2022-09-19 23:03:03 +0900

    Initial commit

---
+++ src/main/java/com/sp/fc/springbootsecuity/SpringBootSecuityApplication.java
@@ -0,0 +1,13 @@
+package com.sp.fc.springbootsecuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecuityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sp/fc/springbootsecuity/SpringBootSecuityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.fc.springbootsecuity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecuityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
